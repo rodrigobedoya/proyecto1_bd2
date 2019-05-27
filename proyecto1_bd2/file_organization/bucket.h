a50d9d965ec739161973d53e07c772a16faf63be
Joe-08 <joe_andre0710@hotmail.com>
2019-05-26 20:13:32 -0500

    Comentando el hash

--- proyecto1_bd2/file_organization/bucket.h
+++ proyecto1_bd2/file_organization/bucket.h
@@ -6,31 +6,28 @@
 using namespace std;
 
 class Bucket {
-    int depth, size;
-    map<int, long> values;
+    int depth, size; // profundidad y tamaño del bucket
+    map<int, long> values; // mapa del key y values
 
     public:
-        Bucket(int depth, int size) : depth(depth), size(size) {};
+        Bucket(int depth, int size) : depth(depth), size(size) {}; //Constructor del bucket
         
-        long search(int key) {
+        long search(int key) { // funcion search 
             auto it = values.find(key);
-            if (it != values.end())
-            {
+            if (it != values.end()) { // si la key esta en el bucket
                 cout << "Valor = " << it->second << endl;
                 return it->second;
-            }
-            else
-            {
+            } else { // caso contrario
                 cout << "Key no existe\n";
                 return -1;
             }
         }
 
-        int insert(int key,long value) {
+        int insert(int key,long value) { // funcion insert
             auto it = values.find(key);
-            if (it != values.end())
+            if (it != values.end()) // si key existe
                 return -1;
-            if (isFull())
+            if (isFull()) // si el bucket está lleno
                 return 0;
             values[key] = value;
             return 1;
@@ -38,52 +35,52 @@ class Bucket {
         
         bool update(int key, long value) {
             auto it = values.find(key);
-            if (it != values.end()) {
+            if (it != values.end()) { // si key existe
                 values[key] = value;
                 cout << "Valor actualizado\n";
                 return true;
-            } else {
-                cout << "No se puede actualizar: key no existe\n";
+            } else { // caso contrario
+                cout << "No se puede actualizar, key no existe\n";
                 return false;
             }
         }
         
-        void display() {
-            for (auto it = values.begin(); it != values.end(); it++)
+        void display() { // funcion display
+            for (auto it = values.begin(); it != values.end(); it++) // imprime las keys en un bucket
                 cout << it->first << " ";
             cout << endl;
         }
         
-        map<int, long> copy() {
-            map<int, long> temp(values.begin(), values.end());
+        map<int, long> copy() { // funcion copy
+            map<int, long> temp(values.begin(), values.end()); // crea una copia del mapa values
             return temp;
         }
 
-        bool isFull() {
-            if (values.size() == size)
+        bool isFull() { // funcion isFull
+            if (values.size() == size) // si el bucket esta lleno retorna true
                 return true;
-            return false;
+            return false; // caso contrario 
         }
         
-        bool isEmpty() {
-            if (values.size() == 0)
+        bool isEmpty() { // funcion isEmpty
+            if (values.size() == 0) // si el bucket no tiene keys retorna true
                 return true;
-            return false;
+            return false; // caso contrario
         }
         
-        int getDepth() { return depth; }
+        int getDepth() { return depth; } // retorna profundidad de bucket actual
         
-        int incrementDepth() {
-            depth++;
+        int incrementDepth() { // funcion incrementDepth
+            depth++; // aumenta profundidad
             return depth;
         }
         
-        int decrementDepth() {
-            depth--;
+        int decrementDepth() { // funcion decrementDepth
+            depth--; // disminuye profundidad
             return depth;
         }
         
-        void clear() { values.clear(); }
+        void clear() { values.clear(); } // borra todo el mapa 
         
 };
 #endif
--- proyecto1_bd2/file_organization/directory.h
+++ proyecto1_bd2/file_organization/directory.h
@@ -7,26 +7,25 @@
 #include "file_organization/bucket.h"
 
 class Directory {
-    int global_depth, bucket_size;
-    vector<Bucket*> buckets;
+    int global_depth, bucket_size; // profundidad global y tamaño de los buckets
+    vector<Bucket*> buckets; // vector que apunta a los buckets
 
-    int hash(int n) { return n & ((1<<global_depth) - 1); }
+    int hash(int n) { return n & ((1<<global_depth) - 1); } // funcion hash
     
-    int pairIndex(int bucket_no, int depth) { return bucket_no ^ (1<<(depth - 1)); }
+    int pairIndex(int bucket_no, int depth) { return bucket_no ^ (1<<(depth - 1)); } // 
     
-    void grow() {
-        for (int i = 0; i < 1<<global_depth; i++)
+    void grow() { // funcion grow
+        for (int i = 0; i < 1<<global_depth; i++) // aumentar buckets
             buckets.push_back(buckets[i]);
-        global_depth++;
+        global_depth++; // aumenta profundidad global
     }
     
-
-    void split(int bucket_no) {
+    void split(int bucket_no) { // funcion split
         int local_depth, pair_index, index_diff, dir_size;
         map<int, long> temp;
 
         local_depth = buckets[bucket_no]->incrementDepth();
-        if (local_depth > global_depth)
+        if (local_depth > global_depth) // incrementar profundidad global si un bucket tiene una mas profunda
             grow();
         
         pair_index = pairIndex(bucket_no, local_depth);
@@ -36,15 +35,15 @@ class Directory {
         index_diff = 1<<local_depth;
         dir_size = 1<<global_depth;
 
-        for (int i = pair_index - index_diff; i >= 0; i-=index_diff)
+        for (int i = pair_index - index_diff; i >= 0; i-=index_diff) 
             buckets[i] = buckets[pair_index];
-        for (int i = pair_index + index_diff; i < dir_size; i+=index_diff)
+        for (int i = pair_index + index_diff; i < dir_size; i+=index_diff) 
             buckets[i] = buckets[pair_index];
-        for (auto it = temp.begin(); it != temp.end(); it++)
+        for (auto it = temp.begin(); it != temp.end(); it++) // insertar antiguas key en nuevos buckets
             insert((*it).first, (*it).second, 1);
     }
     
-    string bucket_id(int n) {
+    string bucket_id(int n) { // funcion bucket_id
         int d;
         string s;
         d = buckets[n]->getDepth();
@@ -62,48 +61,48 @@ class Directory {
     }
 
     public:
-        Directory(int depth, int bucket_size) : global_depth(depth), bucket_size(bucket_size) {
+        Directory(int depth, int bucket_size) : global_depth(depth), bucket_size(bucket_size) { // Constructor del Directory 
             for (int i = 0; i < 1<<depth ; i++)
                 buckets.push_back(new Bucket(depth, bucket_size));
         }
         
-        int insert(int key, long value, bool reinserted) {
-            int bucket_no = hash(key);
+        int insert(int key, long value, bool inserted) { // funcion insert
+            int bucket_no = hash(key); // bucar posicion de la key
             int status = buckets[bucket_no]->insert(key, value);
-            if (status == 1) {
-                if (!reinserted)
+            if (status == 1) { // si no esta key en bucket
+                if (!inserted) 
                     cout << "Key insertada " << key << " en bucket " << bucket_id(bucket_no) << endl;
-                else
+                else // al hacer split y mover key
                     cout << "Key movida " << key << " a bucket " << bucket_id(bucket_no) << endl;
-            } else if (status == 0) {
+            } else if (status == 0) { // si bucket esta lleno
                 split(bucket_no);
-                insert(key, value, reinserted);
-            } else {
+                insert(key, value, inserted);
+            } else { // si ya existe key en el bucket
                 cout << "Key " << key << " ya existe en bucket " << bucket_id(bucket_no) << endl;
                 return 1;
             }
             return 0;
         }
         
-        void update(int key, long value) {
-            int bucket_no = hash(key);
-            buckets[bucket_no]->update(key, value);
+        void update(int key, long value) { // funcion update
+            int bucket_no = hash(key); // buscar posicion de la key
+            buckets[bucket_no]->update(key, value); // actualizar valor de ka key
         }
         
-        long search(int key) {
-            int bucket_no = hash(key);
+        long search(int key) { // funcion search
+            int bucket_no = hash(key); // buscar posicion de la key
             cout << "Buscando key " << key << " en bucket " << bucket_id(bucket_no) << endl;
-            long pos = buckets[bucket_no]->search(key);
-            cout << "pos = "<<pos<<endl;
+            long pos = buckets[bucket_no]->search(key); // posicion del bucket
+            cout << "pos = " << pos << endl;
             return pos;
         }
         
-        void display(bool duplicates) {
+        void display(bool duplicates) { // funcion display
             int d;
             string s;
             set<string> shown;
             cout << "Global depth: " << global_depth << endl;
-            for (int i = 0; i < buckets.size(); i++) {
+            for (int i = 0; i < buckets.size(); i++) { // imprime el hash table y los buckets con sus keys
                 d = buckets[i]->getDepth();
                 s = bucket_id(i);
                 if (duplicates || shown.find(s) == shown.end()) {
